abb5e69ef1da1750d3c23daa392d8eb23124a4f4
equipe 2 de 2024 <126794519+equipe3sesi@users.noreply.github.com>
2024-04-05 08:43:23 -0300

    nao entendi nada 05/04

---
+++ boto rosa.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	 char c = 'a';
+	 int x = 5;
+	 float f = 5.25;
+	 double d = 15.673;
+	 double y = 5.0e10;
+	 
+	 int numeros [5] = {1,2,3,4,5};
+for (size_t i = 0; i < 4; i++)
+{
+	    printf("%d \n", numeros[i]);
+}
+	printf("O valor é c =%c \n", c);
+	printf("O valor é x =%x \n", x);
+	printf("O valor é f =%f \n", f);
+	printf("O valor é d =%d \n", d);
+	printf("O valor é y =%y \n", y);
+}
+	
+	
+	
